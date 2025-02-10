0935632808834237784c9dce681467a7af9a7444
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-02-10 10:22:30 -0500

    ♻️ refactor(results.service): enhance find methods to support additional relations for countries and regions

--- server/researchindicators/src/domain/entities/announcement-settings/dto/update-announcement-setting.dto.ts
+++ server/researchindicators/src/domain/entities/announcement-settings/dto/update-announcement-setting.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAnnouncementSettingDto } from './create-announcement-setting.dto';
 
-export class UpdateAnnouncementSettingDto extends PartialType(CreateAnnouncementSettingDto) {}
+export class UpdateAnnouncementSettingDto extends PartialType(
+  CreateAnnouncementSettingDto,
+) {}
