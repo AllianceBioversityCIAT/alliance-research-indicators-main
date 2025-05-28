02ec5554f439c836e6f2100532132f3134409dc8
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-05-28 10:29:15 -0500

    ♻️ feat(green-checks): enhance GreenChecksModule and service with OpenSearch integration and new ResultRepository

--- server/researchindicators/src/domain/entities/report-year/report-year.module.ts
+++ server/researchindicators/src/domain/entities/report-year/report-year.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReportYearService } from './report-year.service';
 import { ReportYearController } from './report-year.controller';
+import { ReportYearRepository } from './repositories/report-year.repository';
 
 @Module({
   controllers: [ReportYearController],
-  providers: [ReportYearService],
+  providers: [ReportYearService, ReportYearRepository],
   exports: [ReportYearService],
 })
 export class ReportYearModule {}
