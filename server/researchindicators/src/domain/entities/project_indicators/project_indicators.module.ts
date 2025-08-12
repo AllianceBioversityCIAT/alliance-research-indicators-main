6584a36f5138d961d789ea02a51c5bdb5aa19422
Natalia Higuita <n.higuita@cgiar.org>
2025-08-12 09:41:14 -0500

    ✨ feat: implement CreateProjectIndicator DTO, service methods for fetching and creating project indicators, and update controller with new endpoints

--- server/researchindicators/src/domain/entities/project_indicators/project_indicators.module.ts
+++ server/researchindicators/src/domain/entities/project_indicators/project_indicators.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProjectIndicatorsService } from './project_indicators.service';
 import { ProjectIndicatorsController } from './project_indicators.controller';
+import { ProjectIndicator } from './entities/project_indicator.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ProjectIndicatorsController],
   providers: [ProjectIndicatorsService],
+  imports: [TypeOrmModule.forFeature([ProjectIndicator])],
 })
 export class ProjectIndicatorsModule {}
