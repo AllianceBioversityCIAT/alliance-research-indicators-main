19274412fc8a59f72a4f156d064af50fc5b0f791
Natalia Higuita <n.higuita@cgiar.org>
2025-08-15 08:51:00 -0500

    ♻️ refactor: clean up imports and remove unused methods in Project Indicators Results module

--- server/researchindicators/src/domain/entities/project_indicators_results/project_indicators_results.controller.ts
+++ server/researchindicators/src/domain/entities/project_indicators_results/project_indicators_results.controller.ts
@@ -1,15 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProjectIndicatorsResultsService } from './project_indicators_results.service';
-import { CreateProjectIndicatorsResultDto } from './dto/create-project_indicators_result.dto';
-import { UpdateProjectIndicatorsResultDto } from './dto/update-project_indicators_result.dto';
+import {
+  Controller,
+} from '@nestjs/common';
 
 @Controller('project-indicators-results')
 export class ProjectIndicatorsResultsController {
-  constructor(private readonly projectIndicatorsResultsService: ProjectIndicatorsResultsService) {}
-
-  @Post()
-  create(@Body() createProjectIndicatorsResultDto: CreateProjectIndicatorsResultDto) {
-    return this.projectIndicatorsResultsService.create(createProjectIndicatorsResultDto);
-  }
-
 }
