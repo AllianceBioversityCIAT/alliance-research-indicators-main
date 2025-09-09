dc948c21140543be250dff6c2a1532664de598ca
davinzifc <47566127+davinzifc@users.noreply.github.com>
2025-09-09 15:47:08 -0500

    ✨ feat(result-oicr): add OICR_ALIGNMENT role and update related services and tests

--- server/researchindicators/src/domain/entities/result-oicr/result-oicr.service.ts
+++ server/researchindicators/src/domain/entities/result-oicr/result-oicr.service.ts
@@ -231,7 +231,7 @@ export class ResultOicrService {
         resultId,
         allLevers,
         'lever_id',
-        LeverRolesEnum.ALIGNMENT,
+        LeverRolesEnum.OICR_ALIGNMENT,
         manager,
         ['is_primary'],
       );
