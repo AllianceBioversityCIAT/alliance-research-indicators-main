c1ab995318a672a97eca9bfadfe4ad1ba61e88d4
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2025-09-16 17:24:00 -0500

    ✨ feat: update ResultCapacitySharing and TEMP_external_oicrs entities to modify date columns and add new fields

--- server/researchindicators/src/domain/entities/temp_external_oicrs/entities/temp_external_oicr.entity.ts
+++ server/researchindicators/src/domain/entities/temp_external_oicrs/entities/temp_external_oicr.entity.ts
@@ -39,10 +39,10 @@ export class TempExternalOicr extends AuditableEntity {
 
   @Column({
     type: 'text',
-    name: 'pdf_url',
+    name: 'handle_link',
     nullable: true,
   })
-  pdf_url: string;
+  handle_link: string;
 
   @Column({
     type: 'text',
@@ -50,4 +50,53 @@ export class TempExternalOicr extends AuditableEntity {
     nullable: true,
   })
   external_id: string;
+
+  @Column({
+    type: 'text',
+    name: 'main_contact_person_list',
+    nullable: true,
+  })
+  main_contact_person_list: string;
+
+  @Column({
+    type: 'text',
+    name: 'elaboration_narrative',
+    nullable: true,
+  })
+  elaboration_narrative: string;
+
+  @Column({
+    type: 'text',
+    name: 'lever_list',
+    nullable: true,
+  })
+  lever_list: string;
+
+  @Column({
+    type: 'bigint',
+    name: 'geo_scope_id',
+    nullable: true,
+  })
+  geo_scope_id: number;
+
+  @Column({
+    type: 'text',
+    name: 'country_list',
+    nullable: true,
+  })
+  country_list: string;
+
+  @Column({
+    type: 'text',
+    name: 'region_list',
+    nullable: true,
+  })
+  region_list: string;
+
+  @Column({
+    type: 'text',
+    name: 'geo_scope_comment',
+    nullable: true,
+  })
+  geo_scope_comment: string;
 }
