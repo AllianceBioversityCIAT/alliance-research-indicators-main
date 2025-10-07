b861f70aaaf6030d48e7a5ac6e1a0c81c7c409b6
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2025-10-07 10:29:36 -0500

    ✨ feat(alliance-user-staff-groups): add groupId query parameter to findAllMap method for filtering by Staff Group ID

--- server/researchindicators/src/domain/entities/alliance-user-staff-groups/alliance-user-staff-groups.controller.ts
+++ server/researchindicators/src/domain/entities/alliance-user-staff-groups/alliance-user-staff-groups.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
 import { AllianceUserStaffGroupsService } from './alliance-user-staff-groups.service';
 import { BaseController } from '../../shared/global-dto/base-controller';
 import { ResponseUtils } from '../../shared/utils/response.utils';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { isEmpty } from '../../shared/utils/object.utils';
+import { StaffGroupsEnum } from '../staff-groups/enum/staff-groups.enum';
 
+@ApiTags('Alliance User Staff')
+@ApiBearerAuth()
 @Controller()
 export class AllianceUserStaffGroupsController extends BaseController<AllianceUserStaffGroupsService> {
   constructor(service: AllianceUserStaffGroupsService) {
     super(service, 'AllianceUserStaffGroup');
   }
 
+  @ApiQuery({
+    name: 'groupId',
+    required: false,
+    type: Number,
+    enum: StaffGroupsEnum,
+    description: 'Filter by Staff Group ID',
+  })
   @Get('map')
-  async findAllMap() {
-    return this.service.findAllMap().then((res) =>
+  async findAllMap(@Query('groupId') groupId?: number) {
+    const groupIdParam = isEmpty(groupId) ? undefined : +groupId;
+    return this.service.findAllMap(groupIdParam).then((res) =>
       ResponseUtils.format({
         data: res,
         description: 'Alliance User Staff Groups mapped by Alliance User Staff',
