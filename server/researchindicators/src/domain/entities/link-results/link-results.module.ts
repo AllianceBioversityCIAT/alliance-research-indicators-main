ffbc36a9c7e6dceaf2ccdae12c314bac8bc5b323
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2025-11-20 09:17:50 -0500

    ♻️ refactor(link-results): integrate ResultContractsModule and enhance LinkResultsService with contract retrieval logic

--- server/researchindicators/src/domain/entities/link-results/link-results.module.ts
+++ server/researchindicators/src/domain/entities/link-results/link-results.module.ts
@@ -2,11 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { LinkResultsService } from './link-results.service';
 import { LinkResultsController } from './link-results.controller';
 import { ResultsModule } from '../results/results.module';
+import { ResultContractsModule } from '../result-contracts/result-contracts.module';
 
 @Module({
   controllers: [LinkResultsController],
   providers: [LinkResultsService],
   exports: [LinkResultsService],
-  imports: [forwardRef(() => ResultsModule)],
+  imports: [forwardRef(() => ResultsModule), ResultContractsModule],
 })
 export class LinkResultsModule {}
