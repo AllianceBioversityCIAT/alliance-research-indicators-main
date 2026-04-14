758e03ea880a5871d7716fe23275979568a8ecb3
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2026-04-14 11:24:39 -0500

    🗑️ chore(dto): remove unused CreateResultLeverSdgTargetDto and UpdateResultLeverSdgTargetDto files to clean up the codebase.

--- server/researchindicators/src/domain/entities/result-lever-sdg-targets/dto/update-result-lever-sdg-target.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateResultLeverSdgTargetDto } from './create-result-lever-sdg-target.dto';
-
-export class UpdateResultLeverSdgTargetDto extends PartialType(
-  CreateResultLeverSdgTargetDto,
-) {}
