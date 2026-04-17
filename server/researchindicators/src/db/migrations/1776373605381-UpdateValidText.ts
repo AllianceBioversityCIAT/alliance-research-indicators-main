57907d2ef97f6dba31ac638b3888267e165536c8
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2026-04-17 08:34:48 -0500

    ✨ feat(entity): update Result entity to include charset and collation settings for the description column, enhancing text handling capabilities.

---
+++ server/researchindicators/src/db/migrations/1776373605381-UpdateValidText.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateValidText1776373605381 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP FUNCTION IF EXISTS valid_text;
+            `);
+        await queryRunner.query(`
+            CREATE FUNCTION \`valid_text\`(text TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci) RETURNS tinyint(1)
+                READS SQL DATA
+                DETERMINISTIC
+            BEGIN
+                 RETURN IF(
+              text IS NOT NULL, 
+              LENGTH(TRIM(REGEXP_REPLACE(text, '\\\\s+', ''))) > 0, 
+              FALSE
+          );
+            END
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP FUNCTION IF EXISTS valid_text;
+        `);
+        await queryRunner.query(`
+            CREATE FUNCTION \`valid_text\`(text TEXT) RETURNS tinyint(1)
+                READS SQL DATA
+                DETERMINISTIC
+            BEGIN
+                RETURN IF(
+              text IS NOT NULL, 
+              LENGTH(TRIM(REGEXP_REPLACE(text, '\\\\s+', ''))) > 0, 
+              FALSE
+          );
+            END
+        `);
+    }
+
+}
+
