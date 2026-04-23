cbc88a338675904809a73129a0e182e30da35c5e
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2026-04-23 07:45:55 -0500

    🐛 fix(prms): remove unnecessary nullish coalescing operator in results return statement for cleaner code.

---
+++ server/researchindicators/src/domain/tools/open-search/prms/dto/prms-response.dto.spec.ts
@@ -0,0 +1,57 @@
+import {
+  ContributingCenterMapper,
+  ContributingPartnerMapper,
+  CountryMapper,
+  CreatedByMapper,
+  EntityMapper,
+  EvidencesMapper,
+  GeographicFocusMapper,
+  IndicatorCategoryMapper,
+  PrmsInitiativeDto,
+  PrmsKnowledgeProductDto,
+  PrmsResponseDto,
+  PrmsResultByInitiativesDto,
+  PrimaryEntityMapper,
+  RegionMapper,
+  ResultLevelMapper,
+  ResultResponseMapper,
+  SearcherResponseDto,
+  SubEntityMapper,
+  TocMapper,
+  TocResultMapper,
+} from './prms-response.dto';
+
+describe('prms-response DTOs', () => {
+  it('should allow constructing exported mapper classes', () => {
+    expect(new PrmsKnowledgeProductDto()).toBeInstanceOf(PrmsKnowledgeProductDto);
+    expect(new PrmsResponseDto()).toBeInstanceOf(PrmsResponseDto);
+    expect(new PrmsResultByInitiativesDto()).toBeInstanceOf(
+      PrmsResultByInitiativesDto,
+    );
+    expect(new PrmsInitiativeDto()).toBeInstanceOf(PrmsInitiativeDto);
+    expect(new ResultLevelMapper()).toBeInstanceOf(ResultLevelMapper);
+    expect(new IndicatorCategoryMapper()).toBeInstanceOf(
+      IndicatorCategoryMapper,
+    );
+    expect(new GeographicFocusMapper()).toBeInstanceOf(
+      GeographicFocusMapper,
+    );
+    expect(new RegionMapper()).toBeInstanceOf(RegionMapper);
+    expect(new CountryMapper()).toBeInstanceOf(CountryMapper);
+    expect(new ContributingCenterMapper()).toBeInstanceOf(
+      ContributingCenterMapper,
+    );
+    expect(new ContributingPartnerMapper()).toBeInstanceOf(
+      ContributingPartnerMapper,
+    );
+    expect(new EvidencesMapper()).toBeInstanceOf(EvidencesMapper);
+    expect(new PrimaryEntityMapper()).toBeInstanceOf(PrimaryEntityMapper);
+    expect(new EntityMapper()).toBeInstanceOf(EntityMapper);
+    expect(new SubEntityMapper()).toBeInstanceOf(SubEntityMapper);
+    expect(new TocResultMapper()).toBeInstanceOf(TocResultMapper);
+    expect(new TocMapper()).toBeInstanceOf(TocMapper);
+    expect(new SearcherResponseDto()).toBeInstanceOf(SearcherResponseDto);
+    expect(new CreatedByMapper()).toBeInstanceOf(CreatedByMapper);
+    expect(new ResultResponseMapper()).toBeInstanceOf(ResultResponseMapper);
+  });
+});
---
+++ server/researchindicators/src/domain/tools/open-search/prms/prms.opensearch.module.spec.ts
@@ -0,0 +1,7 @@
+import { PrmsOpenSearchModule } from './prms.opensearch.module';
+
+describe('PrmsOpenSearchModule', () => {
+  it('should be defined', () => {
+    expect(PrmsOpenSearchModule).toBeDefined();
+  });
+});
