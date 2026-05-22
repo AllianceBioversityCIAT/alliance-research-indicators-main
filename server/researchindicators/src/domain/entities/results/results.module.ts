59ec0e043eba10cf62447dd641b55e0d35fea788
JuanCode <jucacar28@hotmail.com>
2026-05-22 12:56:45 -0500

    Revert "Merge branch 'AC-1594-bilateral-module' into dev"
    This reverts commit e432874aace0dabe90a83d150226f65edf159838, reversing
    changes made to 92c5d43355e0b276bc680737d2bfa11a5b7fd87b.

--- server/researchindicators/src/domain/entities/results/results.module.ts
+++ server/researchindicators/src/domain/entities/results/results.module.ts
@@ -1,8 +1,7 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ResultsService } from './results.service';
 import { ResultsController } from './results.controller';
 import { ResultRepository } from './repositories/result.repository';
-import { ResultOicrModule } from '../result-oicr/result-oicr.module';
 import { ResultKeywordsModule } from '../result-keywords/result-keywords.module';
 import { ResultLeversModule } from '../result-levers/result-levers.module';
 import { ResultContractsModule } from '../result-contracts/result-contracts.module';
@@ -69,7 +68,6 @@ import { GreenChecksModule } from '../green-checks/green-checks.module';
     ResultInnovationDevModule,
     ResultSdgsModule,
     ResultIpRightsModule,
-    forwardRef(() => ResultOicrModule),
     ClarisaCountriesModule,
     ResultInstitutionsModule,
     ResultEvidencesModule,
