20b3e8565ce4bd9b7955c95a677d36cbf06c4ec8
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2026-07-10 09:55:04 -0500

    🔧 refactor(result-oicr): add custom_lever_name field to lever mappings and update service and tests for improved data handling

--- server/researchindicators/src/domain/entities/result-oicr/result-oicr.service.ts
+++ server/researchindicators/src/domain/entities/result-oicr/result-oicr.service.ts
@@ -404,6 +404,7 @@ export class ResultOicrService {
       (lever) => ({
         lever_id: lever?.lever_id,
         is_primary: true,
+        custom_lever_name: lever?.custom_lever_name,
       }),
     );
 
@@ -411,6 +412,7 @@ export class ResultOicrService {
       data.contributor_lever.map((lever) => ({
         lever_id: lever?.lever_id,
         is_primary: false,
+        custom_lever_name: lever?.custom_lever_name,
       }));
 
     const allLevers = [...savePrimaryLevers, ...saveContributorLevers];
@@ -427,7 +429,7 @@ export class ResultOicrService {
       'lever_id',
       LeverRolesEnum.ALIGNMENT,
       manager,
-      ['is_primary'],
+      ['is_primary', 'custom_lever_name'],
     );
   }
 
