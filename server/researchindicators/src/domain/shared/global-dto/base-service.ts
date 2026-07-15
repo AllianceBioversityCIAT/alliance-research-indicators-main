4eb5148595d1cd757035dea84fe0d010375ae006
David Felipe Casañas Hernadez <47566127+davinzifc@users.noreply.github.com>
2026-07-15 14:11:32 -0500

    🔧 refactor(base-service): remove debug logging and streamline data saving process in BaseServiceSimple class

--- server/researchindicators/src/domain/shared/global-dto/base-service.ts
+++ server/researchindicators/src/domain/shared/global-dto/base-service.ts
@@ -188,8 +188,6 @@ export abstract class BaseServiceSimple<
 
     await entityManager.update(updateWhere, inactiveData);
 
-    console.log('newDataToSave ========', newDataToSave);
-
     const finalDataToSave = this.lastRefactoredAfterSave(
       newDataToSave,
       dataRole,
